8dee09a28a21536d0dc227ef68ff534fdb00bf1d
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-03-21 14:51:15 +0000

    [break]bookers

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, length: { minimum: 1, maximum: 500 }, on: :update
+  validates :body, length: { minimum: 1, maximum: 500 }, on: :create
 end
