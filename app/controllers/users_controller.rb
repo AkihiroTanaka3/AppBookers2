f61f1ce64df6053354d471df2cf2f6e3612950cf
AkihiroTanaka3 <qqqwwetyu@gmail.com>
2023-03-22 13:35:20 +0000

    [break]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
-    @user = current_user
-    @book = @user.books
+    @user = User.find(params[:id])
     @newbook = Book.new
-    @books = Book.all
+    @books = @user.books.all
   end
 
   def index
@@ -13,21 +13,31 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
-  
+
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user)
     else
       render :edit
     end
   end
-  
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to book_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true, on: :create
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true, on: :update
-  
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: {maximum: 50 }
 
   has_many :books, dependent: :destroy
 
